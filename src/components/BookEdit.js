a65ee0b9462187a6013d70825971d79d4931320e
SravaniBandirajula123 <Sravani@aadvisolutions.com>
2025-01-10 18:53:38 +0530

    Deleting a book, and editing book details

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,5 +1,23 @@
+import { useState  } from "react";
 function BookEdit() {
-    return <div>BookEdit</div>
+    const [title, setTitle] = useState('');
+
+    const handleChange =(event)=>{
+        setTitle(event.target.value);
+    };
+
+    const handleSubmit =(event)=>{
+        event.preventDefault();
+    }
+    return <form className="book-edit">
+        <label>Title</label>
+        <input className="input" value={title} onChange={handleChange}/>
+        <button className="button is-primary">
+         Save
+        </button>
+
+
+    </form>
 }
 
 export default BookEdit;
